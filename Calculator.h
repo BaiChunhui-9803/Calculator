25ac33de3e16734f28d2db94d1cf00c0cb7b8ed9
alen-chouchou <51870011+alen-chouchou@users.noreply.github.com>
2019-10-12 14:29:05 +0800

    Add files via upload

--- Calculator.h
+++ Calculator.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <math.h>
 using namespace std;
 
 class Calculator {
@@ -14,18 +15,49 @@ private:
 
 	Stack<char> m_opr; // 运算符栈
 
+
+
+
 	// 知识, 得到运算符优先级
 	int precedence(const char &s) const;
 
 	// 读取操作数
 	double readNum(string::const_iterator &it);
 
+	string readLet(string::const_iterator & it);
+
+	string readExp(string::const_iterator & it);
+
 	// 动作, 使用运算符栈和操作数栈顶的元素进行计算并修改两个栈
 	void calculate();
 
 	bool isNum(string::const_iterator &c) const {
 
-		return *c >= '0'&&*c <= '9' || *c == '.';
+		return *c >= '0'&& *c <= '9' || *c == '.';
+
+	}
+
+	bool isLet(string::const_iterator &c) const {
+
+		return *c >= 65 && *c <= 90 || *c >= 97 && *c <= 122;
+
+	}
+
+	bool isBra(string::const_iterator &c) const {
+
+		return *c == 40 || *c == 41 || *c == 91 || *c == 93 || *c == 123 || *c == 125;
+
+	}
+
+	bool isBra_left(string::const_iterator &c) const {
+
+		return *c == 40 || *c == 91 || *c == 123;
+
+	}
+
+	bool isBra_right(string::const_iterator &c) const {
+
+		return *c == 41 || *c == 93 || *c == 125;
 
 	}
 
--- main.cpp
+++ main.cpp
@@ -7,9 +7,27 @@ int main() {
 
 	Calculator cclt;
 
+	cout << "【通用计算器】系统介绍\n" << endl;
+	cout << "1.支持基本运算，如+、-、*、/、^" << endl;
+	cout << "2.支持函数运算，如sin()、cos()、tan()、sqrt()、abs()" << endl;
+	cout << "3.支持括号运算，如()、[]、{}" << endl;
+	cout << "4.支持符号识别，如pi" << endl;
+	cout << "5.支持负数识别，如-2、-sin(1/2*pi)" << endl;
+	cout << "\n请输入表达式" << endl;
+
 	while (getline(cin, exp)) {
 
-		cout << exp << cclt.doIt(exp) << endl;
+	double result = cclt.doIt(exp);
+
+		if (result < 0.00001)
+
+			cout << exp << 0 << endl;
+
+		else
+
+			cout << exp << result << endl;
+
+	cout << "\n请输入表达式" << endl;
 
 	}
 
