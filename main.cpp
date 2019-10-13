e92f2d158c5f770cfb16a977d20366b0833414b3
alen-chouchou <51870011+alen-chouchou@users.noreply.github.com>
2019-10-13 14:27:20 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -8,11 +8,13 @@ int main() {
 	Calculator cclt;
 
 	cout << "【通用计算器】系统介绍\n" << endl;
-	cout << "1.支持基本运算，如+、-、*、/、^" << endl;
+	cout << "1.支持基本运算，如+、-、*、/、^、%" << endl;
 	cout << "2.支持函数运算，如sin()、cos()、tan()、sqrt()、abs()" << endl;
-	cout << "3.支持括号运算，如()、[]、{}" << endl;
-	cout << "4.支持符号识别，如pi" << endl;
-	cout << "5.支持负数识别，如-2、-sin(1/2*pi)" << endl;
+	cout << "3.支持指对运算，如lg()、ln()、e^2" << endl;
+	cout << "4.支持括号运算，如()、[]、{}" << endl;
+	cout << "5.支持符号识别，如pi、e" << endl;
+	cout << "6.支持负数识别，如-2、-sin(1/2*pi)" << endl;
+	cout << "例：ln(e)*[5%2+sin(2-sqrt(abs(-1)))^2]=" << endl;
 	cout << "\n请输入表达式" << endl;
 
 	while (getline(cin, exp)) {
